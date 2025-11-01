fc9654bf4eb6efe8d5ff0ab1dec135a0d67d4df2
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2025-11-01 11:59:11 +0100

    Use brightnessctl instead of blight

--- .config/i3/scripts/volume_brightness.sh
+++ .config/i3/scripts/volume_brightness.sh
@@ -1,30 +1,29 @@
-#!/bin/bash
-# original source: https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
+#!/usr/bin/env bash
+# Original source: https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
 
-# taken from here: https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
-
-# See README.md for usage instructions
 bar_color="#7f7fff"
 volume_step=1
 brightness_step=5
 max_volume=100
-max_brightness=$(blight get max-brightness)
+max_brightness=$(brightnessctl m)
 
-# Uses regex to get volume from pactl
-function get_volume {
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+
+# --- Volume handling -------------------------------------------------------
+
+get_volume() {
     pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1
 }
 
-# Uses regex to get mute status from pactl
-function get_mute {
+get_mute() {
     pactl get-sink-mute @DEFAULT_SINK@ | grep -Po '(?<=Mute: )(yes|no)'
 }
 
-# Returns a mute icon, a volume-low icon, or a volume-high icon, depending on the volume
-function get_volume_icon {
+get_volume_icon() {
+    local volume mute
     volume=$(get_volume)
     mute=$(get_mute)
-    if [ "$volume" -eq 0 ] || [ "$mute" == "yes" ] ; then
+    if [ "$volume" -eq 0 ] || [ "$mute" = "yes" ]; then
         volume_icon=" "
     elif [ "$volume" -lt 50 ]; then
         volume_icon=" "
@@ -33,60 +32,64 @@ function get_volume_icon {
     fi
 }
 
-# Always returns the same icon - I couldn't get the brightness-low icon to work with fontawesome
-function get_brightness_icon {
+show_volume_notif() {
+    local volume
+    volume=$(get_volume)
+    get_volume_icon
+    dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal \
+        "$volume_icon $volume%" -h int:value:"$volume" -h string:hlcolor:"$bar_color"
+}
+
+# --- Brightness handling ---------------------------------------------------
+
+get_brightness() {
+    brightnessctl g
+}
+
+get_brightness_icon() {
     brightness_icon=""
 }
 
-# Displays a volume notification using dunstify
-function show_volume_notif {
-    volume=$(get_mute)
-    get_volume_icon
-    dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
-}
-
-# Displays a brightness notification using dunstify
-function show_brightness_notif {
+show_brightness_notif() {
+    local brightness percent
     brightness=$(get_brightness)
     get_brightness_icon
-    dunstify -t 1000 -r 2593 -u normal "$brightness_icon $((($(blight get) * 100) /max_brightness))%" -h int:value:$brightness -h string:hlcolor:$bar_color
+    percent=$(( brightness * 100 / max_brightness ))
+    dunstify -t 1000 -r 2594 -u normal \
+        "$brightness_icon ${percent}%" -h int:value:"$percent" -h string:hlcolor:"$bar_color"
 }
 
-# Main function - Takes user input, "volume_up", "volume_down", "brightness_up", or "brightness_down"
-case $1 in
+# --- Main -----------------------------------------------------------------
+
+case "$1" in
     volume_up)
-    # Unmutes and increases volume, then displays the notification
-    pactl set-sink-mute @DEFAULT_SINK@ 0
-    volume=$(get_volume)
-    if [ $(( "$volume" + "$volume_step" )) -gt $max_volume ]; then
-        pactl set-sink-volume @DEFAULT_SINK@ $max_volume%
-    else
-        pactl set-sink-volume @DEFAULT_SINK@ +$volume_step%
-    fi
-    show_volume_notif
-    ;;
-
+        pactl set-sink-mute @DEFAULT_SINK@ 0
+        volume=$(get_volume)
+        if [ $((volume + volume_step)) -gt $max_volume ]; then
+            pactl set-sink-volume @DEFAULT_SINK@ $max_volume%
+        else
+            pactl set-sink-volume @DEFAULT_SINK@ +${volume_step}%
+        fi
+        show_volume_notif
+        ;;
     volume_down)
-    # Raises volume and displays the notification
-    pactl set-sink-volume @DEFAULT_SINK@ -$volume_step%
-    show_volume_notif
-    ;;
-
+        pactl set-sink-volume @DEFAULT_SINK@ -${volume_step}%
+        show_volume_notif
+        ;;
     volume_mute)
-    # Toggles mute and displays the notification
-    pactl set-sink-mute @DEFAULT_SINK@ toggle
-    show_volume_notif
-    ;;
-
+        pactl set-sink-mute @DEFAULT_SINK@ toggle
+        show_volume_notif
+        ;;
     brightness_up)
-    # Increases brightness and displays the notification
-    blight set +$brightness_step%
-    show_brightness_notif
-    ;;
-
+        brightnessctl set +${brightness_step}%
+        show_brightness_notif
+        ;;
     brightness_down)
-    # Decreases brightness and displays the notification
-    blight set -$brightness_step%
-    show_brightness_notif
-    ;;
+        brightnessctl set ${brightness_step}-%
+        show_brightness_notif
+        ;;
+    *)
+        echo "Usage: $0 {volume_up|volume_down|volume_mute|brightness_up|brightness_down}"
+        exit 1
+        ;;
 esac
